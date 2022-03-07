6ce0785d3eb34e8b530c7778fdc56639d6c1bfb7
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-03-07 12:56:54 +0530

    lab

--- LINQtoObjectDemo/Program.cs
+++ LINQtoObjectDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQtoObjectDemo
 {
@@ -7,8 +8,53 @@ namespace LINQtoObjectDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Welcome to LINQ to Objects demos");
+            // Case 1: Get all names of employees
+            var case1 = from e1 in Employee.Employees()
+                        select e1.Name;
+
+            // Case 2: Get all names and salary of employees
+            var case2 = from e2 in Employee.Employees()
+                        select new NameSal{ Name = e2.Name, Salary=e2.Salary };
+
+            // Case 3: Get all names and dept from employees
+            var case3 = from e3 in Employee.Employees()
+                        select new  { Name = e3.Name, Dept = e3.Dept };
+
+            // Case 4: Get all employees getting salary more than 50k
+            var case4 = from e4 in Employee.Employees()
+                        where e4.Salary >= 50000
+                        select e4;
+
+            // Case 5: Get all Employees total salary
+
+            // Case 6: Get count of employees working in HR dept
+
+            // Case 7: Get the name of the employee who's getting highest salary
+
+            // Case 8: Get the average salary of employees in bangalore
+
+            // Case 9: Get the dept mininum avg salary
+
+
+
+            foreach (var item in case1)
+            {
+                Console.WriteLine(item);
+            }
         }
+    }
+
+    //class NameDept
+    //{
+    //    public string Name { get; set; }
+    //    public string Dept { get; set; }
+    //}
+
+    class NameSal
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
     }
 
 
