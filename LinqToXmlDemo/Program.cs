89d14750f67b92ede03fd62e4653334a03f0638d
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-03-07 15:39:14 +0530

    xml

---
+++ LinqToXmlDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Linq; // linq to xml
+using System.Linq; // linq to objects
+
+
+namespace LinqToXmlDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {"one","two","three","four","five","six" };
+            // get all short words (<=3)
+            // LINQ to Object
+            var shortWords = from w in words
+                             where w.Length <= 3
+                             select w;
+
+            // load xml document into memory
+            // LINQ to XML
+            XDocument xml = XDocument.Load("XMLFile1.xml");
+
+            var shortWordsXml = from w in xml.Descendants("word")
+                             where w.Value.Length <= 3
+                             select w.Value;
+
+            foreach (var item in shortWordsXml)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ LinqToXmlDemo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace LinqToXmlDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // load xml document
+            XDocument xml = XDocument.Load("Books.xml");
+            // get all book titles
+            var bookTitles = from book in xml.Descendants("title")
+                             select book.Value;
+
+            // get all book titles the price is less than $40
+
+            var bookTitles2 = from book in xml.Descendants("book")
+                              where double.Parse(book.Element("price").Value) >= 40.0
+                              select book.Element("title").Value;
+            
+            foreach (var item in bookTitles2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
