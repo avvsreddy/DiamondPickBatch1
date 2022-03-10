2854788380b4da05fe660cff9bbfb1f8228c64a4
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-03-10 12:48:00 +0530

    books management

---
+++ BooksCatalogManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BooksCatalogManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read all books information from given books.xml file and store into BooksDb using EF core Code First Approach
+
+            // Duration : 30min
+        }
+    }
+}
