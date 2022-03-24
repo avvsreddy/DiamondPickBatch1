4c7962452ecfc92a683e2b48365ec45e1bdb6b75
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-03-24 13:26:02 +0530

    world population api

---
+++ WorldPopulation.API/Models/CountryPopulation.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldPopulation.API.Models
+{
+    public class CountryPopulation
+    {
+        public int CountryPopulationID { get; set; }
+
+        // add properties as per https://www.worldometers.info/world-population/
+
+    }
+}
